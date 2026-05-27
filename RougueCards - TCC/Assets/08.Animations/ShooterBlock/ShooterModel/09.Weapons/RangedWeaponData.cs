8010e6d4c67fb83e65582512c6f164352cf1cc56
PimentelD <166946623+PimentelD@users.noreply.github.com>
2026-05-27 18:39:59 -0300

    shooter

---
+++ RougueCards - TCC/Assets/08.Animations/ShooterBlock/ShooterModel/09.Weapons/RangedWeaponData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapons/Ranged Weapon")]
+public class RangedWeaponData : ScriptableObject
+{
+    [Header("Estatísticas Principais")] //caracteristicas base que se alteram
+    public GameObject bulletPrefab;
+    public float fireRate = 1f;
+    public float range = 10f;
+    public int damage = 10;
+    public float lifetime = 3f;
+
+    [Header("Comportamento do Disparo")] //isso aqui vai mudar os projeteis por tiro e o angulo respectivamente
+    public BulletMoviment BM;
+    public bool DestroyOnContact = true;
+    public int pellets = 1;
+    public float spread = 0f;
+}
